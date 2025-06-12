5ee351adb262e815202aff59e9807e21447a230a
Juan <jhonra@icloud.com>
2025-06-12 17:34:15 -0500

    another attempt

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import { resolve } from 'node:path'
 import { cloudflare } from '@cloudflare/vite-plugin'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
