50c3c2b8b2654abf89e1f99e2fd1797400f1991c
Juan <jhonra@icloud.com>
2025-06-12 17:14:44 -0500

    fix for git builds

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import solidPlugin from 'vite-plugin-solid'
 import tailwindcss from '@tailwindcss/vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import { resolve } from 'node:path'
-import {cloudflare} from "@cloudflare/vite-plugin"
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +12,9 @@ export default defineConfig({
     TanStackRouterVite({ target: 'solid', autoCodeSplitting: true }),
     solidPlugin(),
     tailwindcss(),
-    cloudflare()
+    cloudflare({
+      configPath: 'wrangler.jsonc',
+    }),
   ],
   resolve: {
     alias: {
